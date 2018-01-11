e4dc9778864a4428a33e127e15220a64ab74a26b
Saúl Ibarra Corretgé <saghul@jitsi.org>
2018-01-11 17:30:57 +0100

    WIP - Picture-in-Picture

---
+++ react/features/mobile/picture-in-picture/actionTypes.js
@@ -0,0 +1,23 @@
+/**
+ * The type of redux action to set the PiP mode change event listener.
+ *
+ * {
+ *     type: _SET_PIP_MODE_LISTENER,
+ *     listener: Function
+ * }
+ *
+ * @protected
+ */
+export const _SET_PIP_MODE_LISTENER = Symbol('_SET_PIP_MODE_LISTENER');
+
+/**
+ * The type of redux action which signals that the PiP mode has changed.
+ *
+ * {
+ *     type: PIP_MODE_CHANGED,
+ *     inPipMode: boolean
+ * }
+ *
+ * @public
+ */
+export const PIP_MODE_CHANGED = Symbol('PIP_MODE_CHANGED');
---
+++ react/features/mobile/picture-in-picture/middleware.js
@@ -0,0 +1,120 @@
+// @flow
+
+import { DeviceEventEmitter } from 'react-native';
+import type { Dispatch } from 'redux';
+
+import {
+    APP_WILL_MOUNT,
+    APP_WILL_UNMOUNT
+} from '../../app';
+import {
+    VIDEO_QUALITY_LEVELS,
+    setLastN,
+    setReceiveVideoQuality
+} from '../../base/conference';
+import { pinParticipant } from '../../base/participants';
+import { MiddlewareRegistry } from '../../base/redux';
+
+import {
+    _setListener,
+    pipModeChanged
+} from './actions';
+import {
+    _SET_PIP_MODE_LISTENER,
+    PIP_MODE_CHANGED
+} from './actionTypes';
+
+/**
+ * Middleware that handles Picture-in-Picture mode changes and reacts to them
+ * by dispatching the needed actions for the application to adjust itself to
+ * the mode. Currently the following happens when PiP mode is engaged:
+ *  - any pinned participant is unpinned
+ *  - last N is set to 1
+ *  - received video quality is set to low
+ * All these actions are reversed when PiP mode is disengaged.
+ *
+ * @param {Store} store - Redux store.
+ * @returns {Function}
+ */
+MiddlewareRegistry.register(store => next => action => {
+    switch (action.type) {
+    case _SET_PIP_MODE_LISTENER: {
+        // Remove the current/old listener.
+        const { pipModeListener } = store.getState()['features/pip'];
+
+        if (pipModeListener) {
+            pipModeListener.remove();
+        }
+
+        // Add the new listener.
+        if (action.listener) {
+            DeviceEventEmitter.addListener(
+                'pictureInPictureModeChanged', action.listener);
+        }
+        break;
+    }
+
+    case PIP_MODE_CHANGED:
+        _pipModeChanged(store, action.inPipMode);
+        break;
+
+    case APP_WILL_MOUNT:
+        store.dispatch(
+            _setListener(
+                _onPipModeChange.bind(undefined, store.dispatch)));
+        break;
+
+    case APP_WILL_UNMOUNT:
+        store.dispatch(_setListener(null));
+        break;
+    }
+
+    return next(action);
+});
+
+/**
+ * Handles PiP mode changes. Dispatches the necessary Redux actions for setting
+ * the app layout / behavior to the PiP mode. See above for details.
+ *
+ * @param {Object} store - Redux store.
+ * @param {boolean} inPipMode - The current PiP mode.
+ * @private
+ * @returns {void}
+ */
+function _pipModeChanged({ dispatch, getState }, inPipMode: boolean) {
+    const { audioOnly } = getState()['features/base/conference'];
+
+    if (inPipMode) {
+        // Unpin any pinned participant
+        dispatch(pinParticipant(null));
+
+        // Set last N to 1, unless we are in audio-only mode
+        if (!audioOnly) {
+            dispatch(setLastN(1));
+        }
+
+        // Set the received video quality to low
+        dispatch(setReceiveVideoQuality(VIDEO_QUALITY_LEVELS.LOW));
+    } else {
+        // Set last N back to its original value
+        if (!audioOnly) {
+            dispatch(setLastN(undefined));
+        }
+
+        // Set the received video quality back to high
+        dispatch(setReceiveVideoQuality(VIDEO_QUALITY_LEVELS.HIGH));
+    }
+}
+
+/**
+ * Called by React Native's DeviceEventEmitter API to notify that the PiP mode
+ * has changed. Dispatches the change within the (associated) Redux store.
+ *
+ * @param {Dispatch} dispatch - Redux dispatch function.
+ * @param {Object} event - The PiP mode changed event.
+ * @private
+ * @returns {void}
+ */
+function _onPipModeChange(dispatch: Dispatch<*>, { isInPictureInPictureMode }) {
+    dispatch(pipModeChanged(isInPictureInPictureMode));
+}
